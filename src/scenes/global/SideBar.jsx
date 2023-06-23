41d8781e81c7e2e1586b524673938c4b8e87e3f5
Kevin Kipkoech <mutaikevinkip.05@gmail.com>
2023-06-23 23:22:35 +0300

    feat: dashboard header

--- src/scenes/global/SideBar.jsx
+++ src/scenes/global/SideBar.jsx
@@ -16,6 +16,7 @@ import PieChartOutlineOutlinedIcon from "@mui/icons-material/PieChartOutlineOutl
 import TimelineOutlinedIcon from "@mui/icons-material/TimelineOutlined";
 import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
 import MapOutlinedIcon from "@mui/icons-material/MapOutlined";
+import profileImage from "../../assets/user.png"
 
 const Item = ({title, to, icon, selected, setSelected}) => {
   const theme = useTheme()
@@ -89,9 +90,12 @@ const SideBar = () => {
                 <Box 
                   display="flex" 
                   justifyContent="center"
+                  width="100px"
+                  height="100px"
+                  margin="auto"
                   alignItems="center">
                     <img
-                      src={`../../../assets/user.png`}
+                      src={profileImage}
                       alt="profile-user"
                       width="100%"
                       height="100%"
