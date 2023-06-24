fba3a21b4d4a9db092d48cf15734a5dc4ddc27ef
Kevin Kipkoech <mutaikevinkip.05@gmail.com>
2023-06-24 22:21:10 +0300

    fix: calendar library bugs

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,12 @@ import Team from "./scenes/team"
 import Invoices from "./scenes/invoices"
 import Contacts from "./scenes/contacts"
 import Bar from "./scenes/bar"
-import Form from "./scenes/dashboard"
+import Form from "./scenes/form"
 import Line from "./scenes/line"
 import Pie from "./scenes/pie"
 import FAQ from "./scenes/faq"
 import Geography from "./scenes/geography"
-//import Calendar from "./scenes/calendar/calendar"
+import Calendar from "./scenes/calendar/calendar"
 
 function App() {
   const [theme, colorMode] = useMode()
@@ -37,7 +37,7 @@ function App() {
               <Route path="/line" element={<Line />}/>
               <Route path="/faq" element={<FAQ />}/>
               <Route path="/geography" element={<Geography />}/>
-              {/* <Route path="/calendar" element={<Calendar />}/> */}
+              <Route path="/calendar" element={<Calendar />}/>
             </Routes>
           </main>
         </div>
