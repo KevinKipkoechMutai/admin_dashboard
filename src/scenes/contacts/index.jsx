96f2436afe12fb00c4f4ebe22d2e3b8c24aaa0fd
Kevin Kipkoech <mutaikevinkip.05@gmail.com>
2023-06-24 17:01:00 +0300

    feat: components and themes

---
+++ src/scenes/contacts/index.jsx
@@ -0,0 +1,14 @@
+import { Box, useTheme } from "@mui/material"
+import { DataGrid, GridToolbar } from "@mui/x-data-grid"
+import { tokens } from "../../theme"
+import { mockDataContacts } from "../../data/mockData"
+import Header from "../../components/Header"
+
+const Contacts = () => {
+    
+  return (
+    <div>index</div>
+  )
+}
+
+export default Contacts
